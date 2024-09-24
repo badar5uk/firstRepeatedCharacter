cbf12cbc4cb2e5bf34f37b8e33021f37958c27b6
bader <bader5uk@hotmail.com>
2024-09-24 13:22:59 +0400

    code uses booleans to check if value exists

---
+++ firstRepeatedChar.java
@@ -0,0 +1,23 @@
+public class firstRepeatedChar {
+
+    public static void main(String[] args) {
+
+        String word = "successs";
+        char[] stringtoChar = word.toCharArray();
+        boolean[] booleans = new boolean[26];
+        for (char checker : stringtoChar) {
+            int index = checker - 'a';
+            if (booleans[index] == true) {
+                System.out.println("Found " + checker);
+                break;
+            }
+            if (booleans[index] != true) {
+                booleans[index] = true;
+            }
+        }
+    }
+}
+
+
+
+
